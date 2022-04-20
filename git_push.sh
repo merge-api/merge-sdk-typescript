7f764af30fd61d83f3fc9a0dedc31fb3e49fa3b2
leewang0 <lee@merge.dev>
2022-04-20 18:32:43 -0400

    change repo and package name, fix example

--- git_push.sh
+++ git_push.sh
@@ -19,7 +19,7 @@ if [ "$git_user_id" = "" ]; then
 fi
 
 if [ "$git_repo_id" = "" ]; then
-    git_repo_id="merge-sdk-node"
+    git_repo_id="merge-sdk-typescript"
     echo "[INFO] No command line input provided. Set \$git_repo_id to $git_repo_id"
 fi
 
