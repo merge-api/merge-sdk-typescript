a60abc5dd3872891e28dea19ff02fcf913d84e71
leewang0 <lee@merge.dev>
2022-07-25 12:43:00 -0400

    wip

---
+++ src/merge_json.ts
@@ -0,0 +1,12 @@
+type JSONValue =
+    | string
+    | number
+    | boolean
+    | JSONObject
+    | JSONArray;
+
+interface JSONObject {
+    [x: string]: JSONValue;
+}
+
+interface JSONArray extends Array<JSONValue> { }
